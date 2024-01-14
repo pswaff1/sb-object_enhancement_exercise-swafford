cf7305ec6e6a611f8f0ecc17fc0fa7990b794ce3
pswaff1 <104938748+pswaff1@users.noreply.github.com>
2024-01-14 03:44:22 -0500

    Implement exercises

--- exercise.js
+++ exercise.js
@@ -27,3 +27,49 @@ function createInstructor(firstName, lastName) {
 // instructor[favoriteNumber] = "That is my favorite!"
 
 // Computed Property Names ES2015
+var favoriteNumber = 42;
+
+var instructor = {
+    firstName: "Colt",
+    [favoriteNumber]: "That is my favorite!"
+}
+
+// Object Methods
+// var instructor = {
+//     firstName: "Colt",
+//     sayHi: function() {
+//         return "Hi!";
+//     },
+//     sayBye: function() {
+//         return this.firstName + " says bye!"; 
+//     }
+// }
+
+// Object Methods ES2015
+const instructor = {
+    firstName: "Colt",
+    sayHi() {
+        return "Hi!";
+    },
+    sayBye() {
+        return this.firstName + " says bye!";
+    }
+}
+
+// createAnimal function
+const d = createAnimal("dog", "bark", "Woooof!")
+// {species: "dog", bark: ƒ}
+d.bark()  //"Woooof!"
+
+const s = createAnimal("sheep", "bleet", "BAAAAaaaa")
+// {species: "sheep", bleet: ƒ}
+s.bleet() //"BAAAAaaaa"
+
+function createAnimal(species, verb, noise) {
+    return {
+        species,
+        [verb]() {
+            return noise;
+        }
+    }
+}
